f3f6de9aa70041616acaec37b129890a5549cee3
DoomKitty87 <doomkitty887@gmail.com>
2023-08-23 10:43:16 -0700

    dictionary

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     <script>
       function courseInfo(courseid) {
         document.getElementById("classInfo").innerHTML = "Loading...";
-        fetch(`classinfo.php?id=${courseid}`, method="GET")
+        fetch(`classinfo.php?id=${courseid}`, { method: "GET" })
           .then(response => response.text())
           .then(data => classInfo.innerHTML = data);
       }
