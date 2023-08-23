4029d97eb498e4adc6f76b73bcb4b64e89f00872
DoomKitty87 <doomkitty887@gmail.com>
2023-08-23 11:07:59 -0700

    ????

--- classinfo.php
+++ classinfo.php
@@ -3,6 +3,6 @@
   $usertoken = $_GET['token'];
   $response = file_get_contents("https://lms.pps.net/api/v1/courses/$courseid?access_token=$usertoken");
   $coursedata = json_decode($response, false);
-  $courseInfo = "Students in course: $coursedata->total_students<br><br>";
+  $courseInfo = "Students in course: $coursedata->total_students";
   echo $courseInfo;
 ?>
