8c653cd10528caa2740c397de2f92c0674d266b1
DoomKitty87 <doomkitty887@gmail.com>
2023-08-23 10:59:50 -0700

    :(

--- classinfo.php
+++ classinfo.php
@@ -1,9 +1,11 @@
 <?php
   $courseid = $_GET['id'];
   $usertoken = $_GET['token'];
+  echo "https://lms.pps.net/api/v1/courses/$courseid/users/self/progress?access_token=$usertoken";
   $response = file_get_contents("https://lms.pps.net/api/v1/courses/$courseid/users/self/progress?access_token=$usertoken");
+  //echo $response;
   $coursedata = json_decode($response, false);
   $coursepercent = $coursedata.requirement_completed_count / $coursedata.requirement_count;
   $courseInfo = "Progress to completion: $coursepercent%";
-  echo $courseInfo;
+  //echo $courseInfo;
 ?>
